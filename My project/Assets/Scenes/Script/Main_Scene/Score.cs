261af864b633d940c4c01a90f7e67a2126772782
LCB <ckdqja5815@naver.com>
2022-12-12 23:08:49 +0900

    V2.0

--- My project/Assets/Scenes/Script/Main_Scene/Score.cs
+++ My project/Assets/Scenes/Script/Main_Scene/Score.cs
@@ -10,24 +10,25 @@ public class Score : MonoBehaviour
     TextMeshProUGUI hightext;
     public static int scoreinAmount = 0;
 
-    public static int highscore = 0;
+    public static int highscore=0;
     public string KeyString = "HighScore";
     
     void Start()
     {
         text = GetComponent<TextMeshProUGUI>();
         hightext = GetComponent<TextMeshProUGUI>();
-        highscore = PlayerPrefs.GetInt(KeyString,0);
-        hightext.text = "High Score: " + highscore.ToString("0");
+        highscore = PlayerPrefs.GetInt(KeyString,highscore);
+        //hightext.text = "High Score: " + highscore.ToString("0");
+        hightext.text = "High Score: " + highscore;
     }
 
     void Update()
     {
         text.text = scoreinAmount.ToString();
-        highscore = 0;
+        //highscore = 0;
         if(scoreinAmount > highscore)
         {
-            PlayerPrefs.SetInt(KeyString, scoreinAmount);
+            PlayerPrefs.SetInt(KeyString, highscore);
         }
     }
 }
